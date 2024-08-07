a51b7f04e2b5174af933cc111093521d1c2344ad
Anthony Gambale <anthony.gambale@anu.edu.au>
2024-08-07 13:04:12 +1000

    Filled in type signatures

--- planner/src/search/fixed_method/fixed_method_tests.rs
+++ planner/src/search/fixed_method/fixed_method_tests.rs
@@ -4,6 +4,6 @@ use super::*;
 #[cfg(test)]
 #[test]
 pub fn my_first_test() {
-  let x: i32 = my_first_function(None, None, None, None, None);
+  let x: i32 = my_first_function();
   assert_eq!(x, 2);
 }
--- planner/src/search/fixed_method/mod.rs
+++ planner/src/search/fixed_method/mod.rs
@@ -1,24 +1,91 @@
-use std::collections::HashSet;
 use super::*;
-use crate::{domain_description::{ClassicalDomain, DomainTasks, FONDProblem}, task_network::Method};
+use crate::{
+    domain_description::{ClassicalDomain, DomainTasks, FONDProblem},
+    task_network::Method,
+};
 use search_graph::*;
+use std::{
+    cell::RefCell,
+    collections::{HashMap, HashSet},
+    rc::Rc,
+    string,
+};
 
 mod fixed_method_tests;
 
-// testing that I am able to run tests
-pub fn my_first_function(
-    tn: Option<HTN>,
-    method: Option<Method>,
-    domain: Option<DomainTasks>,
-    state: Option<HashSet<i32>>,
-    problem: Option<FONDProblem>,
-) -> i32 {
+pub fn my_first_function() -> i32 {
     1 + 1
 }
 
-pub fn run(
-    problem: &FONDProblem,
-    h: fn(&ClassicalDomain, &HashSet<u32>, &HashSet<u32>) -> f32
-) -> (SearchResult, SearchStats) {
-    panic!()
+pub struct SearchNode {
+    pub tn: HTN,
+    pub state: Rc<HashSet<u32>>,
+    pub progressions: Vec<Rc<SearchNode>>,
+    pub status: NodeStatus,
+    pub depth: u32,
+}
+
+impl SearchNode {
+    /*
+        If another SearchNode has the same hash, then *maybe* they are isomorphic
+        If another SearchNode has a different hash, then *definitely* they are not isomorphic
+    */
+    pub fn maybe_isomorphic_hash(&self) -> u32 {
+        // TODO
+        return 0;
+    }
+
+    pub fn get_successors(&self) -> Vec<Rc<SearchNode>> {
+        // TODO
+        return vec![];
+    }
+
+    pub fn to_string(&self) -> String {
+        // TODO
+        String::from("TODO")
+    }
+}
+
+pub struct SearchSpace {
+    /*
+        SearchNodes in the same bucket are *maybe* isomorphic
+        SearchNodes in different buckets are *definitely not* isomorphic
+        Only need to test a given node with the nodes in the same bucket
+    */
+    pub maybe_isomorphic_buckets: HashMap<u32, Vec<Rc<SearchNode>>>,
+    pub initial_search_node: Rc<SearchNode>,
+}
+
+impl SearchSpace {
+    /*
+        Either finds an isomorphic node or creates a new one
+    */
+    pub fn find_isomorphic(new_node: SearchNode) -> Rc<SearchNode> {
+        // TODO
+        Rc::new(new_node)
+    }
+
+    pub fn to_string(&self) -> String {
+        // TODO
+        String::from("TODO")
+    }
+}
+struct AStarSearch {}
+impl AStarSearch {
+    pub fn run(
+        problem: &FONDProblem,
+        heuristic: fn(&ClassicalDomain, &HashSet<u32>, &HashSet<u32>) -> f32,
+    ) -> (SearchResult, SearchStats) {
+        // Call the search function from here
+        panic!()
+    }
+
+    fn search(
+        problem: &FONDProblem,
+        heuristic: fn(&ClassicalDomain, &HashSet<u32>, &HashSet<u32>) -> f32,
+        search_space: SearchSpace,
+        is_goal: fn(&SearchNode, &SearchSpace) -> bool,
+    ) -> (SearchResult, SearchStats) {
+        panic!()
+    }
 }
